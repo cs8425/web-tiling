02c73d485f4938d797311be6b5a389e20be42206
cs8425 <cs8425@gmail.com>
2018-06-07 02:43:43 +0800

    update for any website

--- main.js
+++ main.js
@@ -21,8 +21,12 @@ function updateIFrame() {
 	iframe.width(w).height(h)
 }
 
-function vid2url(vid) {
-	return 'https://www.youtube.com/embed/' + vid + '?autoplay=1'
+function tryUrl(url) {
+	var vid = url.match(/v=([a-zA-Z0-9\-_]+)/)
+	if (url.match('youtube.com/') && (vid.length == 2)) {
+		return 'https://www.youtube.com/embed/' + vid[1] + '?autoplay=1'
+	}
+	return url
 }
 
 function init(){
@@ -37,13 +41,8 @@ function init(){
 	var goUrl = function(e){
 		var header = $(this).parent()
 		var url = header.find('input.url').val()
-		var vid = url.match(/v=([a-zA-Z0-9\-_]+)/)
-		if (vid.length != 2) {
-			return
-		}
-		vid = vid[1]
-		console.log('goto()', e, this, url, vid)
-		header.parent().find('iframe').attr('src', vid2url(vid))
+		console.log('goto()', e, this, url)
+		header.parent().find('iframe').attr('src', tryUrl(url))
 	}
 	var closeTab = function(e){
 		var tab = $(this).parent().parent()
@@ -59,21 +58,16 @@ function init(){
 		$('.modal').css('display', 'none')
 		var input = $(this).parent().find('input')
 		var url = input.val()
-		var vid = url.match(/v=([a-zA-Z0-9\-_]+)/)
-		if (vid.length != 2) {
-			return
-		}
-		vid = vid[1]
 		input.val('')
 		//console.log('open-tab()', e, this, url, vid)
 
 		var tmpl = $('#tmpl > div.window').clone()
 		tmpl.appendTo($('div.block.content'))
-		tmpl.find('iframe').attr('src', vid2url(vid))
+		tmpl.find('iframe').attr('src', tryUrl(url))
 		tmpl.find('input.url').val(url)
 		tmpl.find('span.go.btn').on('click', goUrl)
 		tmpl.find('span.close.btn').on('click', closeTab)
-		console.log('open-tab()', url, vid, vid2url(vid))
+		console.log('open-tab()', url, tryUrl(url))
 
 		updateIFrame()
 
@@ -86,6 +80,6 @@ function init(){
 
 $(window).on('load', function(e) {
 	init()
-	//updateIFrame()
+	updateIFrame()
 })
 
